bf98015cc4ddb428224ade9e217229d4212788f3
LemonFace0309 <charles.liu0309@gmail.com>
2021-01-08 03:25:22 -0500

    styled search page

--- src/components/SearchPage/SearchResult/SearchResult.js
+++ src/components/SearchPage/SearchResult/SearchResult.js
@@ -16,23 +16,23 @@ function SearchResult({
   return (
     <div className={classes.searchResult}>
       <img src={img} alt="" />
-      <FavoriteBorderIcon className="searchResult__heart" />
+      <FavoriteBorderIcon className={classes.searchResult__heart} />
 
-      <div className="searchResult__info">
-        <div className="searchResult__infoTop">
+      <div className={classes.searchResult__info}>
+        <div className={classes.searchResult__infoTop}>
           <p>{location}</p>
           <h3>{title}</h3>
           <p>____</p>
           <p>{description}</p>
         </div>
-        <div className="searchResult__infoBottom">
-          <div className="searchResult__stars">
-            <StarIcon className="searchResult__star" />
+        <div className={classes.searchResult__infoBottom}>
+          <div className={classes.searchResult__stars}>
+            <StarIcon className={classes.searchResult__star} />
             <p>
               <strong>{star}</strong>
             </p>
           </div>
-          <div className="searchResults__price">
+          <div className={classes.searchResults__price}>
             <h2>{price}</h2>
             <p>{total}</p>
           </div>
