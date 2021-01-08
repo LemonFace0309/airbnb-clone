d311b7ca3565fe3151064a2ed2acd5dfc3a73892
LemonFace0309 <charles.liu0309@gmail.com>
2021-01-08 03:07:24 -0500

    hard coded a few places to stay in airbnb

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -3,17 +3,20 @@ import SearchIcon from '@material-ui/icons/Search'
 import LanguageIcon from '@material-ui/icons/Language'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { Avatar } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 import classes from './Header.module.css'
 
 function Header() {
   return (
     <div className={classes.header}>
+      <Link to='/'>
       <img
         className={classes.header__icon}
         src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-Logo.png"
         alt=""
       />
+      </Link>
 
       <div className={classes.header__center}>
         <input type="text"/>
