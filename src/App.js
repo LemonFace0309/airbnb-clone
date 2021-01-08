d4f508c43ad4b069d2ff281262531813ab8f0f52
LemonFace0309 <charles.liu0309@gmail.com>
2021-01-08 02:09:42 -0500

    added a footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import './App.css'
 import Header from './shared/Header/Header'
 import Home from './components/Home/Home'
+import SearchPage from './components/SearchPage/SearchPage'
 import Footer from './shared/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-
-      {/* Search Page */}
-      {/* ... */}
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path='/search' exact component={SearchPage} />
+          <Route path='/' component={Home} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/SearchPage/SearchPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './SearchPage.module.css'
+
+function SearchPage() {
+  return (
+    <div className={classes.searchPage}>
+      <h1>Search Page</h1>
+    </div>
+  )
+}
+
+export default SearchPage
