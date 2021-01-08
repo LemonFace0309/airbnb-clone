e0efadfbef416fc6c1779b4dc794e5dd24a4da32
LemonFace0309 <charles.liu0309@gmail.com>
2021-01-07 19:21:26 -0500

    created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import './static/css/App.css'
+import Header from './Header'
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>LET'S GOOOOOO</h1>
+      <Header />
+      <Home />
+      {/* Home */}
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* Search Page */}
+      {/* ... */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './static/css/Home.css'
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Home Component</h1>
+    </div>
+  )
+}
+
+export default Home
